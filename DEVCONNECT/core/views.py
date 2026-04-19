fa88470eff499e245e360253646eeaa3fbbf8ae7
NISHKARSHrj <ronindas00@gmail.com>
2026-04-19 22:42:59 +0530

    adding the post get endpoint

--- DEVCONNECT/core/views.py
+++ DEVCONNECT/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import Post 
+from .serializers import PostSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+
+@api_view(["GET"])
+def get_posts(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
