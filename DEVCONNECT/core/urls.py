03bc17d414e529fd39cd57afd1a4e72ed8cf193c
NISHKARSHrj <ronindas00@gmail.com>
2026-04-19 22:56:53 +0530

    adding the posts post endpoint

--- DEVCONNECT/core/urls.py
+++ DEVCONNECT/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("posts/", views.get_posts)
+   
+    path("posts/create/", views.posts)
 ]
--- DEVCONNECT/core/views.py
+++ DEVCONNECT/core/views.py
@@ -6,8 +6,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 # Create your views here.
 
-@api_view(["GET"])
-def get_posts(request):
-    posts = Post.objects.all()
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
+@api_view(["POST"])
+def posts(request):
+    serializer = PostSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
