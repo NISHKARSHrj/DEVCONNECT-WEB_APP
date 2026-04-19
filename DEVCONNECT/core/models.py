220002525dd51c8d0004dbb5e6f94d1d1febebe8
NISHKARSHrj <ronindas00@gmail.com>
2026-04-19 22:07:48 +0530

    adding the admin panel

--- DEVCONNECT/core/models.py
+++ DEVCONNECT/core/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 # Create your models here.
 
 
-from django.contrib.auth.models import User
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
